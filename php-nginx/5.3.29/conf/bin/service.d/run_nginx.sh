58da6da2469a39037d727302a5bb3b4351fc7a4c
超级手艺人 <fjxmljc007@163.com>
2020-03-31 18:06:16 +0800

    优化php5.3.29与php5.3.29+nginx

--- php-nginx/5.3.29/conf/bin/service.d/run_nginx.sh
+++ php-nginx/5.3.29/conf/bin/service.d/run_nginx.sh
@@ -7,4 +7,7 @@ fi
 
 /usr/local/sbin/php-fpm -D
 chown www-data:www-data $PHP_FPM_SOCK
+if [[ -f "/etc/nginx/conf.d/default.conf" ]]; then
+	mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
+fi
 exec /usr/sbin/nginx -g 'daemon off;' $SERVICE_NGINX_OPTS
