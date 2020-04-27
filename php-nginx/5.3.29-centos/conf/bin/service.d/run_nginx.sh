67cd23b6f18fe92b84ffa58887c20ede4fd9cfa1
超级手艺人 <fjxmljc007@163.com>
2020-04-27 18:53:04 +0800

    添加php5.3.29-fpm-nginx-centos构建

---
+++ php-nginx/5.3.29-centos/conf/bin/service.d/run_nginx.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Init vars
+if [[ -z "$SERVICE_NGINX_OPTS" ]]; then
+	SERVICE_NGINX_OPTS=""
+fi
+
+/usr/local/sbin/php-fpm -D
+chown www-data:www-data $PHP_FPM_SOCK
+chmod 666 $PHP_FPM_SOCK
+exec /usr/sbin/nginx -g 'daemon off;' $SERVICE_NGINX_OPTS
